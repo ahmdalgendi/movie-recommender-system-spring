3535084830736ae8227c39ed94b791818b59f98b
ahmedalgendi <ahmd.algendi@gmail.com>
2022-08-13 20:14:58 +0200

    added lesson 4

--- src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson1/CollaborativeFilter.java
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson1/CollaborativeFilter.java
@@ -1,5 +1,9 @@
 package io.datajek.spring.basics.movierecommendersystem.lesson1;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+@Primary
+@Component
 public class CollaborativeFilter implements Filter {
     public String[] getRecommendations(String movie) {
         //logic of collaborative filter
